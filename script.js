b3169343b24f54ac27a1f1e8022b366efd66d67a
Ravi Konda <ravi.konda@monitoris.co.uk>
2025-04-17 17:45:04 +0100

    Refactor fetch request in search button event handler for consistency and clarity

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ document.getElementById("searchButton").addEventListener("click", function () {
   fetch("https://script.google.com/macros/s/AKfycbzaC5MuASkvc46Od3Ev2V1DiFGkcVDnvckbfm-w_r0fqPqbW3MV9axCYgC1pW36wEm-/exec", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      type: 'search', // or 'admin'
-      products: ['apples', 'milk']
+      type: "search",
+      products: ["milk", "bread"]
     })
   })
-  .then(response => response.json())
-  .then(data => console.log('RESULT:', data))
-  .catch(err => console.error('ERROR:', err));
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(err => console.error("Fetch error:", err));
 });
